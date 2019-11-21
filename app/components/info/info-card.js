002a5f9c03114765a5935f9ddfde165e4bf8a053
Aad Versteden <madnificent@gmail.com>
2019-11-21 18:32:35 +0100

    Automatic hiding of info cards

--- app/components/info/info-card.js
+++ app/components/info/info-card.js
@@ -1,5 +1,22 @@
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { task } from 'ember-concurrency-decorators';
+import { timeout } from 'ember-concurrency';
 import Card from './card';
 
 export default class InfoInfoCardComponent extends Card {
   defaultTitle = "Info";
+
+  @service("info") infoService;
+
+  @action
+  scheduleRemoval(){
+    this.remove.perform();
+  }
+
+  @task({restartable: true})
+  *remove(){
+    yield timeout(4000);
+    this.infoService.removeCard( this.args.info );
+  }
 }
