2900ada811881907905920b08d728950faf4acac
Aad Versteden <madnificent@gmail.com>
2019-11-21 12:55:56 +0100

    Basic card animations
    The cards are now animated in a basic way.  We will improve the
    behaviour after we implement autoremove for info cards.

--- app/components/info.js
+++ app/components/info.js
@@ -1,6 +1,23 @@
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
+import move from 'ember-animated/motions/move';
+import { fadeOut, fadeIn } from 'ember-animated/motions/opacity';
 
 export default class InfoComponent extends Component {
   @service info;
+
+  *transition({keptSprites, removedSprites, insertedSprites}) {
+    for( const sprite of keptSprites )
+      move(sprite);
+
+    for( const sprite of removedSprites ) {
+      sprite.applyStyles({"z-index": -1});
+      fadeOut(sprite);
+    }
+
+    for( const sprite of insertedSprites ) {
+      fadeIn( sprite );
+      sprite.applyStyles({"z-index": 1});
+    }
+  }
 }
